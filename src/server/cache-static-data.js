a80883932deb04a0d2ccaab3f0109ffe8d1d3c6d
cn.shperry <cn.shperry@gmail.com>
2018-05-03 14:32:19 +0800

    remove testing code.

--- src/server/cache-static-data.js
+++ src/server/cache-static-data.js
@@ -1,11 +1,5 @@
-import _ from 'lodash'
-
 import StoreGlobalDataUtil from '../utils/store-global-data-util'
 
 export default async (fastify) => {
   await StoreGlobalDataUtil.storeGloabalCategories(fastify)
-
-  console.log(_.omitBy({sid: 'cn.shperry', dept_id: null}, (v) => {
-    return _.isNull(v)
-  }))
 }
