1c02a8fbc857d7a145deeb348576223432d9059e
shupeipei <cn.shperry@gmail.com>
2019-02-02 17:10:20 +0800

    add mongoose and static plugin.

--- src/config/default.js
+++ src/config/default.js
@@ -1,3 +1,5 @@
+import path from "path"
+
 export default {
   name: 'fastify-starterkit-api[DEV]',
   log: {
@@ -14,8 +16,8 @@ export default {
     'nodemailer',
     'compress',
     'favicon',
-    // 'mongoose',
-    // 'static-server',
+    'mongoose',
+    'static',
     // 'knexjs'
   ],
   plugin: {
@@ -73,6 +75,22 @@ export default {
     },
     compress: {
       global: false
+    },
+    mongoose: {
+      uri: 'mongodb://localhost:27017/myapp',
+      autoIndex: false, // Don't build indexes.
+      reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect.
+      reconnectInterval: 500, // Reconnect every 500ms.
+      poolSize: 10, // Maintain up to 10 socket connections.
+      // If not connected, return errors immediately rather than waiting for reconnect.
+      bufferMaxEntries: 0,
+      connectTimeoutMS: 10000, // Give up initial connection after 10 seconds.
+      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity.
+      family: 4 // Use IPv4, skip trying IPv6.
+    },
+    static: {
+      root: path.join(__dirname, '..', 'static'),
+      prefix: '/static/', // optional: default '/'.
     }
   },
 
--- src/server/load-plugins.js
+++ src/server/load-plugins.js
@@ -4,7 +4,7 @@ export default (fastify) => {
   plugins.forEach(pluginName => {
     logger.info('loading plugin:', pluginName)
 
-    if (config.get(`switches:plugin:${pluginName}`) === false) {
+    if (config.get(`switches:plugin:${pluginName}`) === false) { // 未配置开关默认开启服务.
       logger.warn(pluginName, `服务未开启, 如要使用此服务, 请开启 switches:plugin:${pluginName} 开关.`)
     } else {
       fastify.register(require(`../plugins/${pluginName}`), config.get(`plugin:${pluginName}`))
